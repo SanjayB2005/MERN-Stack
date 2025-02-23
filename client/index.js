14444edeb1a588441dc9085bcceede7f7ba8ca55
SanjayB2005 <bsanjay2595@gmail.com>
2025-02-23 17:02:59 +0530

    refactor: update todo API with mongoose integration and add CRUD operations

--- client/index.js
+++ client/index.js
@@ -14,7 +14,10 @@ mongoose.connect('mongodb://localhost:27017/mernstack')
 // creating a schema
 
 const todoSchema = new mongoose.Schema({
-  title: String,
+  title: {
+    required: true,
+    type: String
+  },
   description: String
 })
 
@@ -22,22 +25,70 @@ const todoSchema = new mongoose.Schema({
 
 const todoModel = mongoose.model('Todo', todoSchema);
 
-app.post('/todos', (req, res) => {    // Create a new todo API
+app.post('/todos', async(req, res) => {    // Create a new todo API
   const  {title, description} = req.body;
-  const newTodo = {
+  /*const newTodo = {
     id: todos.length + 1,
     title,
     description
   };
   todos.push(newTodo);
-  console.log(todos);
+  console.log(todos);*/
+  
+ try {
+  const newTodo = new todoModel({title, description});
+  await newTodo.save();
   res.status(201).json(newTodo);
+  
+ } catch (error) {
+  console.log(error);
+  res.status(500).json({message: error.message});
+ }
   
 })
 
-app.get('/todos', (req, res) => {    // Get all todos API
-  res.json(todos);
+app.get('/todos', async (req, res) => {    // Get all todos API
+  
+  try {
+    const todos = await todoModel.find();
+    res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: error.message});
+  }
 }) 
+
+// to update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const  {title, description} = req.body; 
+    const id = req.params.id;
+    const updatedTodo = await todoModel.findByIdAndUpdate(id, {title, description},{new: true});
+    if(!updatedTodo) {
+      res.status(404).json({message: 'Todo not found'});
+    }
+    res.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: error.message});
+    
+  }
+})
+
+// to delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const id = req. params.id;
+    await todoModel.findByIdAndDelete(id);
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: error.message});
+    
+  }
+})
 
 port = 3000;
 app.listen(port, () => {
