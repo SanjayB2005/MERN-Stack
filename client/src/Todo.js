c7220a3e430e7e0694f0f286134b36eacb32b7d4
SanjayB2005 <bsanjay2595@gmail.com>
2025-02-24 17:03:45 +0530

    feat: enhance Todo component with edit and delete functionality; add CORS support to server

--- client/src/Todo.js
+++ client/src/Todo.js
@@ -1,18 +1,22 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 const Todo = () => {
 
     const [title, setTitle] = useState('') ;
     const [description, setDescription] = useState('');
+    const [editTitle, setEditTitle] = useState('') ;
+    const [editDescription, setEditDescription] = useState('');
     const [todos, setToDos] = useState([]);  
     const [error, setError] = useState([]);  
     const [successMessage, setSuccessMessage] = useState('');  
+    const [editId, setEditId] = useState(-1);  
     const apiUrl = "http://localhost:8000";
 
-    const handleAddTaskBtn = () => {
+    const handleAddTaskBtn = async () => {
+      setError("");
       if(title.trim() !== "" &&  description.trim() !== ""){
         
-        fetch(apiUrl+"/todos",{
+        await fetch(apiUrl+"/todos",{
           method: "POST",
           headers: {
             'Content-Type' : 'application/json'
@@ -21,14 +25,94 @@ const Todo = () => {
         }).then((res) => {
           if(res.ok){
             setToDos([...todos, {title, description}]);
+            setTitle("");
+            setDescription("");
             setSuccessMessage("Task added successfully");
+            setTimeout(() => {
+              setSuccessMessage("");
+            },2000);
           }
           else{
             setError("Error in adding task!!!");
           }
         })
+        .catch(() => {
+          setError("Error in adding task!!!");
+        })
       }
     }
+
+    useEffect(() => {
+      getAllTasks();
+    }, [])
+
+    const getAllTasks = () => {
+      fetch(apiUrl+"/todos")
+      .then((res) => res.json())
+      .then((res) => {
+        setToDos(res);
+      })
+    }
+
+    const handleEdit = (todo) => {
+      setEditId(todo._id);
+      setEditTitle(todo.title);
+      setEditDescription(todo.description);
+    }
+
+    const handleUpdate =  () => {
+      setError("");
+      if(editTitle.trim() !== "" &&  editDescription.trim() !== ""){
+        
+         fetch(apiUrl+"/todos/"+editId,{
+          method: "PUT",
+          headers: {
+            'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({title: editTitle, description: editDescription})
+        }).then((res) => {
+          if(res.ok){
+            const updatedTodos = todos.map(todo => 
+              todo._id === editId 
+                ? {...todo, title: editTitle, description: editDescription}
+                : todo
+            );
+            setToDos(updatedTodos);
+            setSuccessMessage("Task updated successfully");
+            setTimeout(() => {
+              setSuccessMessage("");
+            },2000)
+            setEditId(-1);
+          }
+          else{
+            setError("Error in adding task!!!");
+          }
+        })
+        .catch(() => {
+          setError("Error in adding task!!!");
+        })
+      }
+    }
+
+    const handleEditCancel = () => {
+      setEditId(-1);
+    }
+
+    const handleDelete = (id) => {
+      if(window.confirm("Are you sure you want to delete this task?")){
+        fetch(apiUrl+"/todos/"+id,{
+          method: "DELETE"
+        })
+        .then(() => {
+          const updatedTodos = todos.filter((todo) => todo._id !== id);
+          setToDos(updatedTodos);
+          setSuccessMessage("Task deleted successfully!!");
+          setTimeout(() => {
+            setSuccessMessage("");
+          },2000)
+         
+        })
+    }}
 
   return (
     <Fragment>
@@ -63,6 +147,64 @@ const Todo = () => {
         {error && <p className='max-container mt-4 text-coral-red'>{error}</p>}
         
       </div>
+
+      <div className='max-container mt-6'>
+        <h2 className='font-montserrat text-xl mb-6'>Tasks</h2>
+       {todos.map((todo, index) => 
+          <div key={index} className='flex justify-between items-center bg-gray-100 p-4 mb-4'>
+          <div className='flex flex-col gap'>
+            {
+              editId == -1 || editId !== todo._id ? <>
+                  <span className='font-montserrat font-semibold'>{todo.title}</span>
+                  <span className='font-montserrat mt-4'>{todo.description}</span>
+              </> :
+              <>
+                   <div className='max-container w-full flex justify-center items-center   gap-10'>
+                      <input 
+                      type='text' 
+                      value={editTitle}
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      placeholder='Title'
+                      className='outline-blue-300 px-4 py-2 border border-black rounded-md w-full'
+                      />
+                      <textarea 
+                      value={editDescription}
+                      onChange={(e) => setEditDescription(e.target.value)}
+                      placeholder='Description' 
+                      className='outline-blue-300 border px-4 py-2 border-black rounded-md w-full' 
+                      />
+                    </div>
+
+              </>
+            }
+          </div>
+          <div className='flex gap-4 justify-between'>
+            {
+              editId == -1 || editId !== todo._id ? 
+              <button 
+              className='bg-yellow-300 px-6 py-2 rounded-md ' 
+              onClick={() => handleEdit(todo)}
+              >
+              Edit
+              </button> :
+              <button 
+              className='bg-green-500 px-6 py-2 rounded-md'
+              onClick={handleUpdate}
+              >
+              Update
+              </button>
+            }
+            
+            {
+              editId == -1 ? <button className='bg-coral-red px-6 py-2 rounded-md' onClick={() => handleDelete(todo._id)}>Delete</button> :
+              <button className='bg-coral-red px-6 py-2 rounded-md' onClick={handleEditCancel}>Cancel</button>
+            }
+            
+          </div>
+        </div>
+       )}
+      </div>
+
     </Fragment>
   )
 }
